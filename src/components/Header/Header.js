bfe5bff2d9c9239c0aca4fe200d359a119114a50
arkk200 <arkk20000@gmail.com>
2022-08-25 21:57:11 +0900

    CHORE :: 헤더에 BGCR 글자, 로그인 버튼 배치

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss';
+
+function Header(){
+    return(
+        <header>
+            <div>BGCR</div>
+            <input type="button" value='로그인' />
+        </header>
+    );
+};
+
+export default Header;
