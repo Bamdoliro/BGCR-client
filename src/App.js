63300b2516e714440bd82f3407e4858a2d72ccb6
unknown <sj01048260279@gmail.com>
2022-08-26 22:21:27 +0900

    FIX :: 리뷰 반영 - 변수 카멜케이스로 수정 - scss 중첩 적용

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import "./index.scss"
 function App() {
   return (
     <div className="App">
-      <RankingList 
-        ranking={4}
-        user_name={"김석진"}
-        git_id={"SEOKKAMONI"}
-        commit={144}
-      />
     </div>
   );
 }
--- src/components/Ranking/RankingList.js
+++ src/components/Ranking/RankingList.js
@@ -1,25 +1,25 @@
 import "./rankingList.scss";
 
-function RankingList( {ranking, user_name, git_id, commit} ) {
+function RankingList( {gitRanking, userName, gitId, numberOfCommit} ) {
     return (
         <li className="ranking-list">
             <div className="front-info ">
                 <div className="rank-info">
                     <span className="rank-number">
-                        {ranking}
+                        {gitRanking}
                     </span>
                 </div>
                 <div className="profile">
                     <img className="profile_img" src="https://avatars.githubusercontent.com/u/102217654?v=4" />
                     <div className="info">
-                        <div className="git-id">{git_id}</div>
-                        <div className="user-name">{user_name}</div>
+                        <div className="git-id">{gitId}</div>
+                        <div className="user-name">{userName}</div>
                     </div>
                 </div>
             </div>
-            <div className="commit_info">
+            <div className="commit-info">
                 <span className="commit-number">
-                    <span>{commit} </span>COMMIT
+                    <span>{numberOfCommit} </span>COMMIT
                 </span>
             </div>
         </li>
