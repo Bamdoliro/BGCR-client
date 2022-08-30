eff73c685500294413805cbbff867f3b3a0c67bd
Seola Won <seolalny@gmail.com>
2022-08-30 16:38:14 +0900

    FIX :: CSS 수정(#3)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import TopRanking from "./components/TopRanking/TopRanking";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <TopRanking userName="김한울" userId="gimhanul" gitRank="1" ordNumber="th" commits={1222}/>
-=======
-      <TopRanking userName="김한울" userId="gimhanul" gitrank="1" ordNumber="th" commits={1222}/>
->>>>>>> 2bdb25c68df7ab212291e6079925440565c24859
     </div>
   );
 }
--- src/components/TopRanking/TopRanking.js
+++ src/components/TopRanking/TopRanking.js
@@ -7,7 +7,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
             <li id="Top1-user">
                 <div classname="user-info">
                     <div classname="profile">   
-<<<<<<< HEAD
                         <img id="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
                             <span id="rank-index">
                                 {gitRank}{ordNumber}
@@ -16,16 +15,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
                             <div id="userId">{userId}</div>
                             <div id="userName">{userName}</div>
                             <span id="commits">{commits} COMMIT</span>
-=======
-                        <img classname="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
-                            <span classname="rank-index">
-                                {gitRank}{ordNumber}
-                            </span>
-                        <div classname="profile-info">
-                            <div classname="userId">{userId}</div>
-                            <div classname="userName">{userName}</div>
-                            <span classname="commits">{commits} </span>COMMIT
->>>>>>> 2bdb25c68df7ab212291e6079925440565c24859
                         </div>
                     </div>
                 </div>
@@ -34,7 +23,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
             <li id="Top2-user">
                 <div classname="user-info">
                     <div classname="profile">   
-<<<<<<< HEAD
                         <img id="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
                             <span id="rank-index">
                                 {gitRank}{ordNumber}
@@ -43,16 +31,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
                             <div id="userId">{userId}</div>
                             <div id="userName">{userName}</div>
                             <span id="commits">{commits} COMMIT</span>
-=======
-                        <img classname="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
-                            <span classname="rank-index">
-                                {gitRank}{ordNumber}
-                            </span>
-                        <div classname="profile-info">
-                            <div classname="userId">{userId}</div>
-                            <div classname="userName">{userName}</div>
-                            <span classname="commits">{commits} </span>COMMIT
->>>>>>> 2bdb25c68df7ab212291e6079925440565c24859
                         </div>
                     </div>
                 </div>
@@ -61,7 +39,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
             <li id="Top3-user">
                 <div classname="user-info">
                     <div classname="profile">   
-<<<<<<< HEAD
                         <img id="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
                             <span id="rank-index">
                                 {gitRank}{ordNumber}
@@ -70,16 +47,6 @@ function TopRanking({userName, userId, gitRank, ordNumber, commits}) {
                             <div id="userId">{userId}</div>
                             <div id="userName">{userName}</div>
                             <span id="commits">{commits} COMMIT</span>
-=======
-                        <img classname="profile-img" src="https://avatars.githubusercontent.com/u/80656733?v=4"/>
-                            <span classname="rank-index">
-                                {gitRank}{ordNumber}
-                            </span>
-                        <div classname="profile-info">
-                            <div classname="userId">{userId}</div>
-                            <div classname="userName">{userName}</div>
-                            <span classname="commits">{commits} </span>COMMIT
->>>>>>> 2bdb25c68df7ab212291e6079925440565c24859
                         </div>
                     </div>
                 </div>
