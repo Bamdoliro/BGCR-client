b736fa7b418abce963abd908653f03ca3a31a77e
unknown <sj01048260279@gmail.com>
2022-09-01 20:25:35 +0900

    ADD :: Section 컴포넌트 - Section.js - section.scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./index.scss"
 import Section from "./components/Section/Section"
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Section />
     </div>
   );
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,4 +1,3 @@
-import Header from "../Header/Header";
 import TopRanking from "../TopRanking/TopRanking";
 import RankingList from "../Ranking/RankingList";
 
@@ -6,16 +5,38 @@ import "./section.scss"
 
 function Section() {
     return (
-        <div>
-            <Header />
+        <div className="section-box">
             <TopRanking userName="김한울" userId="gimhanul" gitRank="1" ordNumber="th" commits={1222} />
-            <RankingList
-                gitRanking={1}
-                userName={"김석진"}
-                gitId={"SEOKKAMONI"}
-                numberOfCommit={294}
-                profileImg={"https://avatars.githubusercontent.com/u/102217654?v=4"}
-            />
+            <div className="ranking-ul">
+                <RankingList
+                    gitRanking={1}
+                    userName={"김석진"}
+                    gitId={"SEOKKAMONI"}
+                    numberOfCommit={294}
+                    profileImg={"https://avatars.githubusercontent.com/u/102217654?v=4"}
+                />
+                <RankingList
+                    gitRanking={1}
+                    userName={"김석진"}
+                    gitId={"SEOKKAMONI"}
+                    numberOfCommit={294}
+                    profileImg={"https://avatars.githubusercontent.com/u/102217654?v=4"}
+                />
+                <RankingList
+                    gitRanking={1}
+                    userName={"김석진"}
+                    gitId={"SEOKKAMONI"}
+                    numberOfCommit={294}
+                    profileImg={"https://avatars.githubusercontent.com/u/102217654?v=4"}
+                />
+                <RankingList
+                    gitRanking={1}
+                    userName={"김석진"}
+                    gitId={"SEOKKAMONI"}
+                    numberOfCommit={294}
+                    profileImg={"https://avatars.githubusercontent.com/u/102217654?v=4"}
+                />
+            </div>
         </div>
     )
 }
