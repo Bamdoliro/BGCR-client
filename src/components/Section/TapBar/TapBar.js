cc6b90ace0a4720c5c1862008828fe8b4574b230
unknown <sj01048260279@gmail.com>
2022-09-08 00:27:55 +0900

    SET :: RankingList

--- src/components/Section/TapBar/TapBar.js
+++ src/components/Section/TapBar/TapBar.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 import { useEffect, useState } from 'react';
-=======
-import { useState } from 'react';
->>>>>>> 8ee0b0d38b385c5362e5ba79040bfb046c8a373c
 import ModSwitch from './ModSwitch/ModSwitch';
 import './TapBar.scss';
 
 function TapBar(){
     const [radioValue, setRadioValue] = useState("1");
-<<<<<<< HEAD
     const handleClickRadio = (e) => { setRadioValue(e.target.value); }
     useEffect(() => {
         console.log(radioValue)
     }, [radioValue])
-=======
-    const handleClickRadio = (e) => { console.log(e.target.value); setRadioValue(e.target.value); }
->>>>>>> 8ee0b0d38b385c5362e5ba79040bfb046c8a373c
     return(
         <div id="buttons">
             <div id="taps">
